72c06eeca1ba29820a649352c72f4d2614b6031a
Ariel <ariel120292@gmail.com>
2019-08-27 11:44:38 -0300

    Remove /account from URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
-  {path:'', pathMatch: 'full', redirectTo:'account'},
-  {path:'account', component: AccountComponent}
+  {path:'', pathMatch: 'full', component: AccountComponent}
 ];
 
 @NgModule({
